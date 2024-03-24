332f1beda8262a9fd72a3d2d23c6b1065cc7c2c2
yoon-H <ucando0320@gmail.com>
2024-03-24 18:11:57 +0900

    [UPDATE] add Jump Function
    [UPDATE] add Jump Function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using DG.Tweening;
+using static UnityEngine.GraphicsBuffer;
 
 public class Player : MonoBehaviour
 {
     private E_Direction Dir = E_Direction.Right;
+    private Vector3 LeftLoc = new Vector3(-2,3,0);
+    private Vector3 RightLoc = new Vector3(2,3,0);
+    public Ease ease = Ease.Linear;
 
     // Start is called before the first frame update
     void Start()
@@ -18,20 +24,23 @@ public class Player : MonoBehaviour
         
     }
 
-    void Jump()
+    public void Jump()
     {
-        ChangeDir();
+        SwitchDir();
     }
 
-    void ChangeDir()
+    private void SwitchDir()
     {
         if (Dir == E_Direction.Right) 
         {
             Dir= E_Direction.Left;
+            transform.DOMoveX(LeftLoc.x, 0.15f).SetEase(ease); 
         }
         else
         {
             Dir=E_Direction.Right;
+            transform.DOMoveX(RightLoc.x, 0.15f).SetEase(ease);
         }
     }
+
 }
