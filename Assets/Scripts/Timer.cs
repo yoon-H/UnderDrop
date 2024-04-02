1ea6382f2d1c98421694facbd6aa32148c26a09d
yoon-H <ucando0320@gmail.com>
2024-04-02 14:52:04 +0900

    [UPDATE] add ScoreSystem
    [UPDATE] add ScoreSystem

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class Timer : MonoBehaviour
@@ -9,19 +11,25 @@ public class Timer : MonoBehaviour
     private ObstacleSpawner ObstacleSpawner;
     public GameObject MonsterSpawnerRef;
     private MonsterSpawner MonsterSpawner;
+    public Text ScoreText;
 
     private float ObstacleSpawnCounter = 0f;
     private float MonsterSpawnCounter = 0f;
+    public float ScoreCounter = 0f;
 
     public float ObstacleSpawnTime = 1f;
     public float MonsterSpawnTime = 4f;
 
     private bool IsPaused = false;
 
+    public Int32 Score = 0;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -29,6 +37,14 @@ public class Timer : MonoBehaviour
     {
         ObstacleSpawnCounter += Time.deltaTime;
         MonsterSpawnCounter += Time.deltaTime;
+        ScoreCounter += Time.deltaTime;
+
+        if(ScoreText != null) 
+        {
+            Score = (int)(ScoreCounter / 0.2);
+            ScoreText.text = Score + "m";
+        }
+       
 
         if (ObstacleSpawner == null)
         {
@@ -51,6 +67,7 @@ public class Timer : MonoBehaviour
             MonsterSpawnCounter -= MonsterSpawnTime;
             MonsterSpawner.SpawnMonster();
         }
+
     }
 
     public void SetIsPaused(bool flag)
