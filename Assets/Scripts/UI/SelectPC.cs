31fb921e14b77d6158603a994fce175c1d15413c
yoon-H <ucando0320@gmail.com>
2024-09-02 21:43:50 +0900

    [ADD] change profile when select PC
    [ADD] change profile when select PC

--- Assets/Scripts/UI/SelectPC.cs
+++ Assets/Scripts/UI/SelectPC.cs
@@ -1,4 +1,5 @@
 using Spine;
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,28 @@ public class SelectPC : PlayAnim
 {
     public CharacterInfo Info;
     public SwitchPopUp PopUp;
+    public SkeletonAnimation CharacterImage;
     protected override void Event(TrackEntry entry)
     {
         Info.SelectPC();
         PopUp.SwitchFlag(false);
+
+        if (GameManager.Instance.PlayerIndex == 0)
+        {
+            CharacterImage.Skeleton.SetSkin("Noke");
+            
+        }
+        else
+        {
+            CharacterImage.Skeleton.SetSkin("mont");
+        }
+
+        CharacterImage.Skeleton.SetSlotsToSetupPose();
+        CharacterImage.LateUpdate();
+        CharacterImage.AnimationState.ClearTracks();
+
+        CharacterImage.AnimationState.SetAnimation(0, "animation", false);
+
     }
 
 }
