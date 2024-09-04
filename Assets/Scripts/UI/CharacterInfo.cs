7b9ffe4043cf8d317a76d1638c2838412f2e2653
yoon-H <ucando0320@gmail.com>
2024-09-04 22:41:23 +0900

    [UPDATE] add GameOver Panel Anim
    [UPDATE] add GameOver Panel Anim

--- Assets/Scripts/UI/CharacterInfo.cs
+++ Assets/Scripts/UI/CharacterInfo.cs
@@ -22,12 +22,6 @@ public class CharacterInfo : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnDisable()
     {
         ChangeIndex(GameManager.Instance.PlayerIndex);
